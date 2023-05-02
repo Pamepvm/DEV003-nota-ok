1e5ca2d88dede1bed295775d95d665b6a42c7356
Pamela Viveros <pamviveros@gmail.com>
2023-05-02 13:09:26 -0400

    funcionesfirebase

---
+++ pages/funcionesfirebase.js
@@ -0,0 +1,30 @@
+import { auth } from "./confirebase.js";
+import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+
+export const googleAuth = () => {
+  const provider = new GoogleAuthProvider();
+  signInWithPopup(auth, provider)
+  .then((result) => {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+    // The signed-in user info.
+    const user = result.user;
+    console.log(user,'user')
+    // IdP data available using getAdditionalUserInfo(result)
+    // ...
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.log(errorMessage,'errormensaje')
+    // The email of the user's account used.
+    const email = error.customData.email;
+    // The AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    // ...
+  });
+}
+
+
+
