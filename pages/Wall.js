d9db9aa3939cfe5c2c01804dc79d14fe5ceeeb03
Pamela Viveros <pamviveros@gmail.com>
2023-05-09 23:48:05 -0400

    funciones obtener, guardar y eliminar notas

--- pages/Wall.js
+++ pages/Wall.js
@@ -1,37 +1,74 @@
-import { useState } from 'react'; //permite agregar estados a un componente funcional
-import styles from '../styles/Wall.module.css';
-import { guardarNota } from './funcionesfirebase.js';
+import { useState, useEffect } from "react"; //permite agregar estados a un componente funcional y para bajar textos
+import styles from "../styles/Wall.module.css";
+import {
+  guardarNota,
+  obtenerNotas,
+  eliminarNota,
+} from "./funcionesfirebase.js";
 
 export default function Wall() {
   const [notes, setNotes] = useState([]); // estado de las notas y fun. que setNotes actualiza los datos
-  const [currentNote, setCurrentNote] = useState(''); // agrego el estado para la nota actual
+  const [currentNote, setCurrentNote] = useState(""); // agrego el estado para la nota actual
+  const [fetchedNotes, setFetchedNotes] = useState([]); // crear nuevo estado para matener los textos descargados
 
-  function noteDelete(index) {
-    setNotes(notes.filter((note, i) => i !== index)); //elimino el elemento en posicion index
-  }
+  const obtenerNotasFirebase = async () => {
+    const fetchedData = await obtenerNotas();
+    setFetchedNotes(fetchedData);
+  };
+
+  useEffect(() => {
+    obtenerNotasFirebase();
+  }, []);
+
+  useEffect(() => {
+    setNotes([...fetchedNotes]);
+  }, [fetchedNotes]);
+
+  /*function noteDelete(index) {
+    const newNotes = [...notes];
+    newNotes.splice(index, 1);
+    setNotes(newNotes);
+  }*/
 
   function saveNote() {
-    if (currentNote.trim() !== '') { // si se ingresa algun texto y eliminar los espacios
-      guardarNota(currentNote);// guardo nota en firebase, tomo como argumento el contenido de la nota
-      setCurrentNote(''); // reiniciar el estado currentNote para ingresar otro texto
-      setNotes([...notes, currentNote]);//agrego currentNote a notes y actualizo el estado con la nueva matriz
+    if (currentNote.trim() !== "") {
+      // si se ingresa algun texto y eliminar los espacios
+      guardarNota(currentNote); // guardo nota en firebase, tomo como argumento el contenido de la nota
+      setCurrentNote(""); // reiniciar el estado currentNote para ingresar otro texto
+      //setNotes([...notes, currentNote]); //agrego currentNote a notes y actualizo el estado con la nueva matriz
     }
-  } 
-
-return (
+  }
+  function deleteNote(noteId) {
+    eliminarNota(noteId);
+    const newNotes = notes.filter((note) => note.id !== noteId);
+    setNotes(newNotes);
+  }
+  return (
     <div>
-      <div className={styles.topBar}> <button className={styles.exitButton} onClick={() => { window.location.href = '/'; }}>
-        <img className={styles.exitButton} src="/Images/cancel_presentation_black_24dp.svg" alt="salir"/></button>
-
+      <div className={styles.topBar}>
+        <button
+          className={styles.exitButton}
+          onClick={() => {
+            window.location.href = "/";
+          }}
+        >
+          <img
+            className={styles.exitButton}
+            src="/Images/cancel_presentation_black_24dp.svg"
+            alt="salir"
+          />
+        </button>
       </div>
-        <h1 className={styles.title}>Notas</h1>
-      
+      <h1 className={styles.title}>Notas</h1>
+
       <input
-        className={`${styles.note} ${styles.noteInput}`} placeholder="Escribe una nota" 
-        value={currentNote}// mensaje se ingresa y se guarda en currentNote, para saber el estado del input
-        onChange={(e) => setCurrentNote(e.target.value)}//actualizo el estado
+        className={`${styles.note} ${styles.noteInput}`}
+        placeholder="Escribe una nota"
+        value={currentNote} // mensaje se ingresa y se guarda en currentNote, para saber el estado del input
+        onChange={(e) => setCurrentNote(e.target.value)} //actualizo el estado
         onKeyDown={(e) => {
-          if (e.key === 'Enter') { // verifico si la tecla se activa, si es así se guarda
+          if (e.key === "Enter") {
+            // verifico si la tecla se activa, si es así se guarda
             saveNote();
           }
         }}
@@ -39,19 +76,24 @@ return (
       <button className={styles.saveButton} onClick={saveNote}>
         Guardar Nota
       </button>
-   
-     <div className="notesContainer">
-        {notes.map((note, index) => ( // itero sobre lamatriz notes (argumentos) key identifica de forma única c/ elemento de la lista
-          <div key={index} className={styles.note}> 
-            {note}
-            <button className={styles.deleteButton} onClick={() => noteDelete(index)}>
-              <img className={styles.delete} src="/Images/delete_white_24dp.svg" alt="Delete Note"/>
+
+      <div className={styles.notesContainer}>
+        {notes.map((note) => (
+          <div key={note.id} className={styles.note}>
+            {note.contenido}
+            <button
+              className={styles.deleteButton}
+              onClick={() => deleteNote(note.id)}
+            >
+              <img
+                className={styles.delete}
+                src="/Images/delete_white_24dp.svg"
+                alt="Delete Note"
+              />
             </button>
           </div>
         ))}
-     </div>
+      </div>
     </div>
   );
 }
-
-
--- pages/funcionesfirebase.js
+++ pages/funcionesfirebase.js
@@ -1,47 +1,68 @@
 import { auth, db } from "./confirebase.js";
 import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
-import { getFirestore, collection, addDoc } from "firebase/firestore";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+  doc,
+  deleteDoc,
+} from "firebase/firestore";
 
 export const googleAuth = (router) => {
   const provider = new GoogleAuthProvider();
   signInWithPopup(auth, provider)
-  .then((result) => {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    const credential = GoogleAuthProvider.credentialFromResult(result);
-    const token = credential.accessToken;
-    // The signed-in user info.
-    const user = result.user;
-    console.log(user, 'user')
-    if(user.emailVerified){
-       router.push('/Wall');
-    }
-    // IdP data available using getAdditionalUserInfo(result)
-    // ...
-   if (!user.emailVerified) throw new Error('Email not verified');
-  }).catch((error) => {
-    // Handle Errors here.
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    console.log(errorMessage,'errormensaje')
-    // The email of the user's account used.
-    const email = error.customData.email;
-    // The AuthCredential type that was used.
-    const credential = GoogleAuthProvider.credentialFromError(error);
-    // ...
-  });
-}
+    .then((result) => {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      // The signed-in user info.
+      const user = result.user;
+      console.log(user, "user");
+      if (user.emailVerified) {
+        router.push("/Wall");
+      }
+      // IdP data available using getAdditionalUserInfo(result)
+      // ...
+      if (!user.emailVerified) throw new Error("Email not verified");
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorMessage, "errormensaje");
+      // The email of the user's account used.
+      const email = error.customData.email;
+      // The AuthCredential type that was used.
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      // ...
+    });
+};
 
-export const guardarNota = async (contenido) => {
+export const guardarNota = async (notas) => {
   const db = getFirestore();
-try {
-  const docRef = await addDoc(collection(db, "notas"), {
-    contenido,
-    timestamp: Date.now()
+  try {
+    const docRef = await addDoc(collection(db, "notas"), {
+      contenido: notas,
+      timestamp: Date.now(),
+    });
+    console.log("Document written with ID: ", docRef.id);
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+};
+
+export const obtenerNotas = async () => {
+  const querySnapshot = await getDocs(collection(db, "notas"));
+  const notes = [];
+  querySnapshot.forEach((doc) => {
+    // doc.data() is never undefined for query doc snapshots
+    notes.push({ ...doc.data(), id: doc.id });
   });
-  console.log("Document written with ID: ", docRef.id);
-} catch (e) {
-  console.error("Error adding document: ", e);
-}
-}
+  return notes;
+};
 
-
+export const eliminarNota = (id) => {
+  // console.log('=>', id)
+  deleteDoc(doc(db, "notas, id"));
+};
